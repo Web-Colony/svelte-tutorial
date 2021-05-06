1bb6947597f90995eaee305b56b69732a28527a2
dar4kamal <34961537+dar4kamal@users.noreply.github.com>
2021-05-06 14:11:36 +0200

    added tabs area

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,27 @@
 <script>
-	import Header from "./components/Header.svelte"
-	import Footer from "./components/Footer.svelte"
+import Header from "./components/Header.svelte"
+import Footer from "./components/Footer.svelte"
+import Tabs from "./units/Tabs.svelte"
+
+let tabs = ["Current Polls", "Add New Poll"];
+let activeTab = "Current Polls";
 </script>
 
-<main>		
-	<Header />
-	
-	<Footer />
+<Header />
+<main>
+    <Tabs {activeTab} {tabs} on:tabChange={(e) => activeTab = e.detail}/>
+	{#if activeTab === "Current Polls"}
+	<p> Polls list </p>
+	{:else}
+	<p>Add new Poll</p>
+	{/if}
 </main>
+<Footer />
 
 <style>
-	main {
-		width: 100%;
-		/* max-width: 960px; */
-		/* margin: 40px auto; */
-	}
+main {
+    width: 100%;
+    max-width: 960px;
+    margin: 40px auto;
+}
 </style>
---
+++ src/units/Tabs.svelte
@@ -0,0 +1,46 @@
+<script>
+import {
+    createEventDispatcher
+} from "svelte"
+export let tabs;
+export let activeTab;
+
+const dispatch = createEventDispatcher();
+</script>
+
+<div class="tabs">
+    <ul>
+        {#each tabs as tab}
+        <li on:click={() => dispatch("tabChange", tab)}>
+            <div class:active={tab === activeTab}>{tab}</div>
+        </li>
+        {/each}
+    </ul>
+</div>
+
+<style>
+.tabs {
+    margin-bottom: 40px;
+
+}
+
+ul {
+    display: flex;
+    justify-content: center;
+    padding: 0;
+    list-style-type: none;
+}
+
+li {
+    margin: 0 16px;
+    font-size: 18px;
+    color: #555;
+    cursor: pointer;
+}
+
+.active {
+    color: #d91b42;
+    border-bottom: 2px solid #d91b42;
+    padding-bottom: 8px;
+}
+</style>
