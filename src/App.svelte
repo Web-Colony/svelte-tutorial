729d2fef653ec106ac8f6f0a7ab7ad0890e075fb
dar4kamal <34961537+dar4kamal@users.noreply.github.com>
2021-05-08 11:00:28 +0200

    added pollList component

--- src/App.svelte
+++ src/App.svelte
@@ -3,18 +3,43 @@ import Header from "./components/Header.svelte"
 import Footer from "./components/Footer.svelte"
 import Tabs from "./units/Tabs.svelte"
 import AddPoll from "./components/AddPoll.svelte"
+import PollList from "./components/PollList.svelte";
 
 let tabs = ["Current Polls", "Add New Poll"];
 let activeTab = "Current Polls";
+
+let polls = [{
+    id: 1,
+    question: "How Are you Today ?",
+    answerA: "Fine Thanks",
+    answerB: "Not very well",
+    votesA: 5,
+    votesB: 10 
+
+}];
+const addPoll = (e) => {
+	polls = [e.detail, ...polls];
+	activeTab = "Current Polls";
+}
+
+const handleVote = (e)  => {
+	const { type, id } = e.detail;
+	polls = polls.map(poll => {
+		if(poll.id === id) 
+			poll = type === "a" ? {...poll, votesA: poll.votesA+1 } : {...poll, votesB: poll.votesB+1 }
+		return poll;
+	})
+}
+
 </script>
 
 <Header />
 <main>
     <Tabs {activeTab} {tabs} on:tabChange={(e) => activeTab = e.detail}/>
 	{#if activeTab === "Current Polls"}
-	<p> Polls list </p>
+	 <PollList {polls} on:vote={handleVote}/>
 	{:else}
-	<AddPoll />
+	<AddPoll on:addpoll={addPoll}/>
 	{/if}
 </main>
 <Footer />
@@ -23,6 +48,6 @@ let activeTab = "Current Polls";
 main {
     width: 100%;
     max-width: 960px;
-    margin: 40px auto;
+    margin: 40px auto;	
 }
 </style>
---
+++ src/components/PollDetails.svelte
@@ -0,0 +1,81 @@
+<script>
+import {
+    createEventDispatcher
+} from "svelte";
+
+import Card from "../units/Card.svelte";
+
+export let poll;
+const dispatch = createEventDispatcher();
+
+$: totalVotes = poll.votesA + poll.votesB;
+$: percentA = Math.round(poll.votesA / totalVotes * 100)
+$: percentB = Math.round(poll.votesB / totalVotes * 100)
+const handleVote = (type, id) => {
+    dispatch("vote", {
+        type,
+        id
+    })
+}
+</script>
+
+<Card>
+    <div class="poll">
+        <h3>{poll.question}</h3>
+        <p>Total Votes : {totalVotes}</p>
+        <div class="answer" on:click={() => handleVote("a", poll.id)}>
+            <div class="percent percent-a" style="width: {percentA}%"></div>
+            <span>{poll.answerA} ({poll.votesA})</span>
+        </div>
+        <div class="answer" on:click={() => handleVote("b", poll.id)}>
+            <div class="percent percent-b" style="width: {percentB}%"></div>
+            <span>{poll.answerB} ({poll.votesB})</span>
+        </div>
+    </div>
+</Card>
+
+<style>
+h3 {
+    margin: 0 auto;
+    color: #555;
+}
+
+p {
+    margin-top: 6px;
+    font-size: 14px;
+    color: #aaa;
+    margin-bottom: 30px;
+}
+
+.answer {
+    background: #fafafa;
+    cursor: pointer;
+    margin: 10px auto;
+    position: relative;
+}
+
+.answer:hover {
+    opacity: 0.6;
+}
+
+span {
+    display: inline-block;
+    padding: 10px 20px;
+}
+
+.percent {
+    height: 100%;
+    position: absolute;
+    box-sizing: border-box;
+}
+
+.percent-a {
+    background: rgba(232, 117, 141, 0.3);
+    border-left: 4px solid rgba(232, 117, 141);
+}
+
+.percent-b {
+    background: rgba(143, 219, 191,0.3);
+    border-left: 4px solid rgb(143, 219, 191);
+}
+</style>
